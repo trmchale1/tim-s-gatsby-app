9a79eb376db7d10d2e141bb0ec7ad6b25b69a4be
trmchale1 <trmchale@gmail.com>
2024-02-18 20:38:47 -0700

    left side styling set up, missing bio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,25 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
 
 
 // Improve the left side with background and bio
 // Improve the right side by connecting a live updating application with new blog posts and media
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
+    
     
     
     <div className="split-screen">
       <div className="left-side">
-      <h1 style={headingStyles}>
-        Good Morning Tim!
-      </h1>
+      <h1>Good Morning Tim</h1>
       </div>
       
       <div className="right-side">
         <p>What is going on over here?</p>
       </div>
     </div>
-    </main>
+    
   )
 }
 
