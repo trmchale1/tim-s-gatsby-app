5d752b22764c08e26a125c52bb5d65f6a34c9345
trmchale1 <trmchale@gmail.com>
2024-02-19 19:16:10 -0700

    getting gist data and displaying it

--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -4,10 +4,18 @@ import { useEffect, useState } from "react"
 
 export function Post({ post }) {
     const getUserData = async () => {  
-        var gitHubUrl = `https://api.github.com/users/trmchale1`;
+        var gh_key = "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";
+        var gitHubUrl = `https://api.github.com/users/trmchale1/gists`;
         const response = await fetch(gitHubUrl);
         const jsonData = await response.json();
-        console.log(jsonData)
+        // how do we abstract this md file, make it generic for all .md files
+        console.log(jsonData[0]["files"]["fallback.md"]["raw_url"])
+        const gist_raw = jsonData[0]["files"]["fallback.md"]["raw_url"]
+
+        // we are getting the raw content of the md file, what to do with it?
+        // how many characters to include in a summary?
+        // can we get the title of the gist?
+        
     };
     getUserData();
     return (
