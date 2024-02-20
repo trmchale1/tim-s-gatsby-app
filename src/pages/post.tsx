7188ba2c70176171f376730e6c7e188d2b4b45c2
trmchale1 <trmchale@gmail.com>
2024-02-19 19:27:05 -0700

    currently printing contents of md file

--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -9,13 +9,15 @@ export function Post({ post }) {
         const response = await fetch(gitHubUrl);
         const jsonData = await response.json();
         // how do we abstract this md file, make it generic for all .md files
-        console.log(jsonData[0]["files"]["fallback.md"]["raw_url"])
-        const gist_raw = jsonData[0]["files"]["fallback.md"]["raw_url"]
-
+        console.log(jsonData[0]["files"]["fallback.md"])
+        const gist_raw_url = jsonData[0]["files"]["fallback.md"]["raw_url"]
+        const gist_raw = await fetch(gist_raw_url);
+        const raw_content = await gist_raw.text();
+        console.log(raw_content);
         // we are getting the raw content of the md file, what to do with it?
         // how many characters to include in a summary?
         // can we get the title of the gist?
-        
+
     };
     getUserData();
     return (
