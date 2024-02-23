7592b0d738a71743408523c30785d4ba28e61733
trmchale1 <trmchale@gmail.com>
2024-02-22 21:35:08 -0700

    some preview complete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,25 +7,41 @@ import { Avatar } from "./avatar.tsx"
 
 const [gist] = useState(data)
 
-// Improve the left side with background and bio
-// Improve the right side by connecting a live updating application with new blog posts and media
+// left side add resume
 const IndexPage: React.FC<PageProps> = () => {
   console.log(data)
 
   return (
     <div className="split-screen">
       <div className="left-side">
+      <div className="my-name"><p>Tim McHale</p></div>
       <Avatar avatar = {data["avatar"]}/>
-      <div className="my-name">Tim McHale</div>
+      <div className="about-me">
+        <br></br>
+        <br></br>
+        <br></br>
+        <p>Currently living that indie dev lifestyle.</p>
+        <br></br>
+        <p>Previously a DevOps Engineer at MarketDial.</p>
+        <br></br>
+        <p>Five years experience as a python dev working for startups and freelance.</p> 
+        <br></br>
+        <br></br>
+        <br></br>
+        <p>Check out what I'm doing on <a href="https://twitter.com/mctim123">twitter</a></p>
+        
+        </div>
       </div>
       
       <div className="right-side">
-        <p>What is going on over here?</p>
-        <Post description = {data["Fallout.md"][0]["description"]} content = {data["Fallout.md"][0]["content"]} link = {data["Fallout.md"][0]["link"]}/>
+        <div className="my-name"><p>What I'm doing</p></div>
+        <br/>
         <br />
+        <Post description = {data["Fallout.md"][0]["description"]} content = {data["Fallout.md"][0]["content"]} link = {data["Fallout.md"][0]["link"]}/>
+        <br/>
         <Post description = {data["fallback.md"][0]["description"]} content = {data["fallback.md"][0]["content"]} link = {data["fallback.md"][0]["link"]} />
+        </div>
       </div>
-    </div>
   )
 }
 
--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -1,12 +1,23 @@
 import * as React from "react"
+import Markdown from 'markdown-to-jsx'
+import OpengraphReactComponent from 'opengraph-react';
 
 export function Post({ description, content, link }) {
 
     return (
-      <div>
-           <p>{description}</p>
-           <p>{content}</p>
-           <p>{link}</p>
-      </div>
+      <div className="my-subject">
+        <p>{description}</p>
+        <br/>
+           <div className="my-text">
+            <Markdown>{content}</Markdown>
+            <br />
+            <OpengraphReactComponent  
+              site={link}  
+              appId={"d0117daf-1d35-47c5-9eff-55f4d3be3da9"}  
+              loader={<p></p>}  
+              size={'medium'}/>
+            </div>
+            </div>
+            
     );
   }
