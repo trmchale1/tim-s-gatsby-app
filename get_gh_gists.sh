d4a23dd78a5ebb421aa3cf4465fcefb59996080a
trmchale1 <trmchale@gmail.com>
2024-02-23 21:49:51 -0700

    added to the shell script, get repo activity

--- get_gh_gists.sh
+++ get_gh_gists.sh
@@ -8,4 +8,13 @@ curl -L \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/trmchale1/tim-s-gatsby-app > GatsbyRepo.json
 
+curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/trmchale1/tim-s-gatsby-app/activity >
+
+  # checkout the events link in GatsbyRepo.json https://api.github.com/repos/trmchale1/tim-s-gatsby-app/events
+  # might some good stuff in there
+  # the third curl has a history of the repo
+
 python3 parse.py
