13fae727a2cdf0ca8fc97abdaf73d8d6e09c9fdb
trmchale1 <trmchale@gmail.com>
2024-02-27 11:03:01 -0700

    one more commit

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { useEffect, useState } from "react"
-import AvatarData from "./json/avatar.json"
+import AvatarData from "../json/avatar.json"
 
 
 export function Avatar({ avatar }) {
