50d7f78d160385e462a1d0b7b7a1758833f20913
Tim <trmchale@gmail.com>
2024-03-03 16:04:50 -0700

    Revert "Revert "Add repo commit history""

--- get_gh_gists.sh
+++ get_gh_gists.sh
@@ -13,6 +13,8 @@ curl -L \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/trmchale1/tim-s-gatsby-app/activity > GatsbyActivity.json
 
+
+
   # checkout the events link in GatsbyRepo.json https://api.github.com/repos/trmchale1/tim-s-gatsby-app/events
   # might some good stuff in there
   # the third curl has a history of the repo
